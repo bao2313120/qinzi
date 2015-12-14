58115411ed43de5984333d00484e6b70d72ef749
鲍传扬 <baochuanyang@magictavern.com>
2015-12-14 21:19:27 +0800

    update like to user like

--- model/Like.js
+++ model/Like.js
@@ -6,22 +6,22 @@ function Like() {
 module.exports = Like;
 
 Like.insertGoods= function(id,goodsid,type,islike,callback){
-    var sql = "insert into like (id,goodsid,type,islike) values (?,?,?,?)";
+    var sql = "insert into userlike (id,goodsid,type,islike) values (?,?,?,?)";
     db.query(sql,[id,goodsid,type,islike],callback);
 }
 
 Like.insertActionPic= function(id,actionid,actionpicid,type,islike,callback){
-    var sql = "insert into like (id,actionid,actionpicid,type,islike) values (?,?,?,?,?)";
+    var sql = "insert into userlike (id,actionid,actionpicid,type,islike) values (?,?,?,?,?)";
     db.query(sql,[id,actionid,actionpicid,type,islike],callback);
 }
 
 Like.getAllGoodsLikeByIdAndIsLike = function(id,islike,callback){
-    var sql = "select * from like where id=? and type=? and islike=?";
+    var sql = "select * from userlike where id=? and type=? and islike=?";
     db.query(sql,[id,Util.SUPPORT_TYPE_GOODS,islike],callback);
 }
 
 Like.getAllGoodsLikeById = function(id,callback){
-    var sql = "select * from like where type=? and id=?";
+    var sql = "select * from userlike where type=? and id=?";
     db.query(sql,[Util.SUPPORT_TYPE_GOODS,id],callback);
 }
 
