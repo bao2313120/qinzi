ddb966924fd7ba9f4997d3584c5cd7afad98cf23
baochuanyang <391514467@qq.com>
2016-01-31 23:47:06 +0800

    aaa

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var SessionStore = require('express-mysql-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var goods = require('./routes/goods');
@@ -12,13 +13,13 @@ var interact = require('./routes/interact');
 var memberaction = require('./routes/memberaction');
 var back = require('./routes/back');
 var sql = require('./routes/sql');
+var config = require('config');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+var sessionStore = new SessionStore(config.mysql_session)
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -33,7 +34,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'keyboard cat',
-  resave: false,
+  resave: true,
+  store:sessionStore,
   saveUninitialized: true
 }));
 
@@ -76,6 +78,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
